fccf37cc958676fa6e58f123858d75cdd037dd5b
nk161690 <90851794+M3ijn@users.noreply.github.com>
2023-03-18 19:50:43 +0700

    taskboard

--- PRN221_Project_QLNS/QLNS/Pages/TaskBoard.cshtml.cs
+++ PRN221_Project_QLNS/QLNS/Pages/TaskBoard.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using QLNS.Data;
+using QLNS.Models;
 
 namespace QLNS.Pages
 {
     public class TaskBoardModel : PageModel
     {
+        private readonly Prn221ProjectQlnsContext _context;
+
+        public TaskBoardModel(Prn221ProjectQlnsContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public List<Models. Task> Pending { get; set; }
+        [BindProperty]
+        public List<Models.Task> Progress { get; set; }
+        [BindProperty]
+        public List<Models.Task> Review { get; set; }
+        [BindProperty]
+        public List<Models.Task> Done { get; set; }
+
         public void OnGet()
         {
-        }
+            Pending = _context.Tasks.Where(_ => _.Status == 0).ToList();
+
+;        }
     }
 }
