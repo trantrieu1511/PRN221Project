44b48d3689ac1c02d386cbacc5d6903fd5bcc3f4
lư nguyễn <lunguyen2k18@gmail.com>
2023-03-19 23:52:46 +0700

    vẫn lỗi chuyển trạng thái

--- PRN221_Project_QLNS/QLNS/Pages/TaskBoard.cshtml.cs
+++ PRN221_Project_QLNS/QLNS/Pages/TaskBoard.cshtml.cs
@@ -10,11 +10,13 @@ namespace QLNS.Pages
 {
     public class TaskBoardModel : PageModel
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly PRN221_Project_QLNSContext _context;
 
-        public TaskBoardModel(PRN221_Project_QLNSContext context)
+        public TaskBoardModel(PRN221_Project_QLNSContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public IList<Models.Task> Pending { get; set; }
@@ -24,50 +26,88 @@ namespace QLNS.Pages
         public List<SelectListItem> Employees { get; set; }
         public async System.Threading.Tasks.Task OnGetAsync()
         {
-            Employees = await _context.Profiles.Select(_ => new SelectListItem
+            string user = _httpContextAccessor.HttpContext.Session.GetString("UserName") ?? "";
+            int role = _httpContextAccessor.HttpContext.Session.GetInt32("role") ?? 0;
+            if (role == 0)
             {
-                Value = _.ProfileId.ToString(),
-                Text = _.FirstName + " " + _.LastName + "/" + _.Email
-            }).ToListAsync();
-            if (_context.Tasks != null)
+                this.RedirectToPage("/Login");
+            }
+            else
             {
-                Pending = await _context.Tasks.Include(_ => _.AssignedNavigation).Where(_ => _.Status == 0).ToListAsync();
-                Progress = await _context.Tasks.Include(_ => _.AssignedNavigation).Where(_ => _.Status == 1).ToListAsync();
-                Review = await _context.Tasks.Include(_ => _.AssignedNavigation).Where(_ => _.Status == 2).ToListAsync();
-                Done = await _context.Tasks.Include(_ => _.AssignedNavigation).Where(_ => _.Status == 3).ToListAsync();
+                Employees = await _context.Profiles.Select(_ => new SelectListItem
+                {
+
+                    Value = _.ProfileId.ToString(),
+                    Text = _.FirstName + " " + _.LastName + "/" + _.Email
+                }).ToListAsync();
+                if (_context.Tasks != null)
+                {
+                    Pending = await _context.Tasks.Include(_ => _.AssignedNavigation).Where(_ => _.Status == 0).ToListAsync();
+                    Progress = await _context.Tasks.Include(_ => _.AssignedNavigation).Where(_ => _.Status == 1).ToListAsync();
+                    Review = await _context.Tasks.Include(_ => _.AssignedNavigation).Where(_ => _.Status == 2).ToListAsync();
+                    Done = await _context.Tasks.Include(_ => _.AssignedNavigation).Where(_ => _.Status == 3).ToListAsync();
+                }
             }
         }
         public async Task<IActionResult> OnPostAdd()
         {
-            Models.Task task = new Models.Task
+            string user = _httpContextAccessor.HttpContext.Session.GetString("UserName") ?? "";
+            int role = _httpContextAccessor.HttpContext.Session.GetInt32("role") ?? 0;
+            if (role != 2)
             {
-                Name = Request.Form["name"],
-                Description = Request.Form["description"],
-                Deadline = DateTime.ParseExact(Request.Form["deadline"], "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
-                Status = 0,
-                Assigned = int.Parse(Request.Form["employee"]),
-            };
-            _context.Tasks.Add(task);   
-            _context.SaveChanges();
-            return RedirectToPage("./Taskboard");
+                return this.RedirectToPage("/Login");
+            }
+            else
+            {
+                Models.Task task = new Models.Task
+                {
+                    Name = Request.Form["name"],
+                    Description = Request.Form["description"],
+                    Deadline = DateTime.ParseExact(Request.Form["deadline"], "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture),
+                    Status = 0,
+                    Assigned = int.Parse(Request.Form["employee"]),
+                };
+                _context.Tasks.Add(task);
+                _context.SaveChanges();
+                return RedirectToPage("./Taskboard");
+            }
+
         }
         public async Task<IActionResult> OnPostEdit()
         {
-            Models.Task task = _context.Tasks.Where(_ => _.TaskId == Request.Form["id"]).FirstOrDefault();
-            task.Name = Request.Form["name"];
-            task.Description = Request.Form["description"];
-            task.Deadline = DateTime.ParseExact(Request.Form["deadline"], "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
-            task.Assigned = int.Parse(Request.Form["employee"]);
-            _context.Tasks.Update(task);
-            _context.SaveChanges();
-            return RedirectToPage("./Taskboard");
+            string user = _httpContextAccessor.HttpContext.Session.GetString("UserName") ?? "";
+            int role = _httpContextAccessor.HttpContext.Session.GetInt32("role") ?? 0;
+            if (role != 2)
+            {
+                return this.RedirectToPage("/Login");
+            }
+            else
+            {
+                Models.Task task = _context.Tasks.Where(_ => _.TaskId == Request.Form["id"]).FirstOrDefault();
+                task.Name = Request.Form["name"];
+                task.Description = Request.Form["description"];
+                task.Deadline = DateTime.ParseExact(Request.Form["deadline"], "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                task.Assigned = int.Parse(Request.Form["employee"]);
+                _context.Tasks.Update(task);
+                _context.SaveChanges();
+                return RedirectToPage("./Taskboard");
+            }
         }
         public async Task<IActionResult> DeleteTask(int id)
         {
-            Models.Task task = _context.Tasks.Where(_ => _.TaskId == id).FirstOrDefault();
-            _context.Tasks.Remove(task);
-            _context.SaveChanges();
-            return RedirectToPage("./Taskboard");
+            string user = _httpContextAccessor.HttpContext.Session.GetString("UserName") ?? "";
+            int role = _httpContextAccessor.HttpContext.Session.GetInt32("role") ?? 0;
+            if (role != 2)
+            {
+                return this.RedirectToPage("/Login");
+            }
+            else
+            {
+                Models.Task task = _context.Tasks.Where(_ => _.TaskId == id).FirstOrDefault();
+                _context.Tasks.Remove(task);
+                _context.SaveChanges();
+                return RedirectToPage("./Taskboard");
+            }
         }
         public async Task<IActionResult> EditTaskStatus(int id, int status)
         {
@@ -75,7 +115,7 @@ namespace QLNS.Pages
             task.Status = status;
             _context.Tasks.Update(task);
             _context.SaveChanges();
-            return RedirectToPage("./Taskboard");
+            return RedirectToPage("./Login");
         }
     }
 }
