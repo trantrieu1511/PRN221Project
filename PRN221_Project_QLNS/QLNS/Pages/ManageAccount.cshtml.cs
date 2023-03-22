645f7b628e2ee625657f52a1e988b3523f954e52
nk161690 <90851794+M3ijn@users.noreply.github.com>
2023-03-22 19:43:33 +0700

    profile

--- PRN221_Project_QLNS/QLNS/Pages/ManageAccount.cshtml.cs
+++ PRN221_Project_QLNS/QLNS/Pages/ManageAccount.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using QLNS.Data;
+using QLNS.Models;
 
 namespace QLNS.Pages
 {
     public class ManageAccountModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+        private readonly PRN221_Project_QLNSContext _context;
+
+		public ManageAccountModel(PRN221_Project_QLNSContext context)
+		{
+			_context = context;
+		}
+
+		public IList<Account> Accounts { get; set; }
+
+		public async System.Threading.Tasks.Task OnGetAsync()
+		{
+			Accounts = await _context.Accounts.ToListAsync();
+		}
     }
 }
--- PRN221_Project_QLNS/QLNS/Pages/Profile.cshtml.cs
+++ PRN221_Project_QLNS/QLNS/Pages/Profile.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using QLNS.Data;
+using QLNS.Models;
 
 namespace QLNS.Pages
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
+        private readonly PRN221_Project_QLNSContext _context;
+
+        public ProfileModel(PRN221_Project_QLNSContext context)
         {
+            _context = context;
+        }
+        [BindProperty]
+        public Profile Profile { get; set; }
+        public Profile Manager { get; set; }
+
+        public async System.Threading.Tasks.Task OnGetAsync(int? id)
+        {
+            Profile = await _context.Profiles.FirstOrDefaultAsync(_ => _.AccountId == id);
+            Manager = await _context.Profiles.FirstOrDefaultAsync(_ => _.ProfileId == Profile.ReportTo);
+        }
+        public async Task<IActionResult> OnPostEdit()
+        {
+            _context.Profiles.Update(Profile);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Profile", new {id = Profile.AccountId});
         }
     }
 }
